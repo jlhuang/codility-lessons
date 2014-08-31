5f0acd3d056ec47a0539989c4406f49f30cc67b6
jlhuang <jlhuang0821@gmail.com>
2014-08-31 16:33:44 +0900

    add the comment

--- lesson 2 : Counting Elements/FrogRiverOne/Solution.java
+++ lesson 2 : Counting Elements/FrogRiverOne/Solution.java
@@ -8,16 +8,20 @@ class Solution {
     public int solution(int X, int[] A) {
         // write your code in Java SE 8
         int N = A.length;
+        // count array of 1 2 3 ... X.
         boolean[] count = new boolean[X+1];
         int position = 0;
         for (int i = 0; i < N; i++) {
+            // count the leaves from 1 to X.
             if (!count[A[i]]) {
                 count[A[i]] = true;
                 position++;
             }
+            // if the leaves appear at every position from 1 to X, return the index.
             if (position == X)
                 return i;
         }
+        // can't jump to the other side of the river.
         return -1;
     }
 }
